fab52f3982a3600181d7d3aa62343f38ec348d39
runnerlt <juozas.pastas@gmail.com>
2017-11-29 01:07:30 +0200

    Initial

--- app/src/main/java/com/example/android/scorekeeper/MainActivity.java
+++ app/src/main/java/com/example/android/scorekeeper/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.android.scorekeeper;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.android.scorekeeper.R;
@@ -11,6 +12,10 @@ public class MainActivity extends AppCompatActivity {
 
     int scoreTeamA = 0;
     int scoreTeamB = 0;
+    int redTeamA = 0;
+    int redTeamB = 0;
+    int yellowTeamA = 0;
+    int yellowTeamB = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,76 +26,92 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Displays the given score for Team A.
      */
-    public void displayForTeamA(int score) {
+    public void displayForTeamA(int score, int yellow, int red) {
         TextView scoreView = findViewById(R.id.team_a_score);
         scoreView.setText(String.valueOf(score));
+
+        Button yellowButton = findViewById(R.id.team_a_yellow);
+        yellowButton.setText(String.valueOf(yellow));
+
+        Button redButton = findViewById(R.id.team_a_red);
+        redButton.setText(String.valueOf(red));
     }
 
     /**
      * Displays the given score for Team B.
      */
-    public void displayForTeamB(int score) {
+    public void displayForTeamB(int score, int yellow, int red) {
         TextView scoreView = findViewById(R.id.team_b_score);
         scoreView.setText(String.valueOf(score));
+
+        Button yellowButton = findViewById(R.id.team_b_yellow);
+        yellowButton.setText(String.valueOf(yellow));
+
+        Button redButton = findViewById(R.id.team_b_red);
+        redButton.setText(String.valueOf(red));
     }
 
     /**
-     * Add 3 points team A
+     * Add goals team A
      */
-    public void addThreeTeamA(View view) {
-        scoreTeamA += 3;
-        displayForTeamA(scoreTeamA);
+    public void addGoalTeamA(View view) {
+        scoreTeamA++;
+        displayForTeamA(scoreTeamA, yellowTeamA, redTeamA);
     }
 
     /**
-     * Add 3 points team B
+     * Add goals team B
      */
-    public void addThreeTeamB(View view) {
-        scoreTeamB += 3;
-        displayForTeamB(scoreTeamB);
+    public void addGoalTeamB(View view) {
+        scoreTeamB++;
+        displayForTeamB(scoreTeamB, yellowTeamB, redTeamB);
     }
 
     /**
-     * Add 2 points team A
+     * Add yellow team A
      */
-    public void addTwoTeamA(View view) {
-        scoreTeamA += 2;
-        displayForTeamA(scoreTeamA);
+    public void addYellowTeamA(View view) {
+        yellowTeamA++;
+        displayForTeamA(scoreTeamA, yellowTeamA, redTeamA);
     }
 
     /**
-     * Add 2 points team B
+     * Add yellow team B
      */
-    public void addTwoTeamB(View view) {
-        scoreTeamB += 2;
-        displayForTeamB(scoreTeamB);
+    public void addYellowTeamB(View view) {
+        yellowTeamB++;
+        displayForTeamB(scoreTeamB, yellowTeamB, redTeamB);
     }
 
     /**
-     * Add 1 points team A
+     * Add red team A
      */
-    public void addFreeTeamA(View view) {
-        scoreTeamA += 1;
-        displayForTeamA(scoreTeamA);
+    public void addRedTeamA(View view) {
+        redTeamA++;
+        displayForTeamA(scoreTeamA, yellowTeamA, redTeamA);
     }
 
     /**
-     * Add 1 points team B
+     * Add red team B
      */
-    public void addFreeTeamB(View view) {
-        scoreTeamB += 1;
-        displayForTeamB(scoreTeamB);
+    public void addRedTeamB(View view) {
+        redTeamB++;
+        displayForTeamB(scoreTeamB, yellowTeamB, redTeamB);
     }
 
 
     /**
      * Reset scores
      */
-    public void resetScores(View view) {
+    public void resetStats(View view) {
         scoreTeamA = 0;
         scoreTeamB = 0;
-        displayForTeamA(scoreTeamA);
-        displayForTeamB(scoreTeamB);
+        redTeamA = 0;
+        redTeamB = 0;
+        yellowTeamA = 0;
+        yellowTeamB = 0;
+        displayForTeamA(scoreTeamA, yellowTeamA, redTeamA);
+        displayForTeamB(scoreTeamB, yellowTeamB, redTeamB);
     }
 
 
